4dd224bc93ddec09c266f6db9b063ed780c1b5df
My_PC <My_PC@Anant.mshome.net>
2018-07-20 04:47:31 +0530

    First Test class added to the project

---
+++ TestProject/src/com/test/one/Test.java
@@ -0,0 +1,10 @@
+package com.test.one;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hi Welcome To The world of coders!!");
+	}
+
+}
